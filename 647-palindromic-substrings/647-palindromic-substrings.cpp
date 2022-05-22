cad7b9619d5252635cc98bd08ddfc531b1a4a484
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-05-23 02:26:08 +0530

    Time: 21 ms (44.04%), Space: 20.2 MB (11.99%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int ans = 0;
+		// dp[i][j] == 1 for every i==j as every char is a pallindromic substring of length 1
+        for(int i=0;i<n;i++){
+            dp[i][i] = 1;
+            ans++;
+        }
+        
+        for(int i=2;i<=n;i++){
+            for(int j=0;j+i<=n;j++){
+                int k = j+i-1;
+                
+                if(i==2 && s[j] == s[k]) {
+                    dp[j][k] = 1;
+                    ans++;
+                }
+                else if(s[j] == s[k] && dp[j+1][k-1]!=0){ 
+                    dp[j][k] = 1;
+                    ans++;
+                }
+            }
+        }        
+        return ans;
+    }
+};
