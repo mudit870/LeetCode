f9d7cbdbcb01c8dbbbab2b93a586c9fe5e0c7e44
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-05-26 23:44:17 +0530

    Time: 7 ms (12.48%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+//         Kernighan's algorithm
         int ans=0;
-        while(n>0){
-        //1&1 is 1 so count will increment by 1, 
-        //if last digit of n is 0 then 0&1 will be 0 and thus count is incremented by 0 i.e. remain same
-            ans+=(n&1);
-            n>>=1;  //right shift is used, 1 digit will be removed from n
+         while(n){
+            int rmsb = n & (-n); //rightmost set bit mask
+            n -= rmsb;
+            ans++;
         }
         return ans;
     }
