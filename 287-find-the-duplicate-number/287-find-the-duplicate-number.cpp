532ce3e6a13432c7c8e48db08e271239953be9d2
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-06-07 11:13:51 +0530

    Time: 123 ms (84.55%), Space: 61.3 MB (61.47%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
-            // as it is in range from 1 to n so we take abs to get right index
             int val=abs(nums[i]);
-            // first time at that index value will be positive
-            if(nums[val]>0)
+            if(nums[val]>0){
                 nums[val]=-nums[val];
-            // if again come to same index, value will be negative , so return this value
-            else{
+            }else{
                 return val;
             }
         }
