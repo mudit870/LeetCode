7b1b54d470b0d443e33d4d49cae60fe3508a83b8
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-06-20 11:00:56 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (96.09%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        //taking stack for keep tracking the order of the brackets..
-        stack <char> st;
-        
-        int len = s.length();
-        for (int i = 0; i<len ; i++)//iterate over each and every elements
-        {
-             //if current element of the string will be opening bracket
-             //then we will just simply push it into the stack
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
-                st.push(s[i]);
-            else if(s[i] == ')' || s[i] == '}' || s[i] == ']')
-            {
-                if(st.empty())
-                    return false;
-                else if(s[i] == ')' && st.top() != '(')
-                    return false;
-                else if(s[i] == '}' && st.top() != '{')
-                    return false;
-                else if(s[i] == ']' && st.top() != '[')
-                    return false;
-                //if control reaches to that line,
-                //it means we have got the right pair of brackets,
-                //so just pop it.
-                else 
-                    st.pop();
-            }
+    bool isValid(string st) {
+        stack<char>s;
+        for(auto e:st){
+            if(s.empty() || e=='[' || e=='{' || e=='(')
+                s.push(e);
+            else if(e==')' && s.top()=='(')
+                s.pop();
+            else if(e==']' && s.top()=='[')
+                s.pop();
+            else if(e=='}' && s.top()=='{')
+                s.pop();
+            else
+                return false;
         }
-        if(st.empty())
-            return true;
-        else
+        if(!s.empty())
             return false;
+        return true;
     }
 };
