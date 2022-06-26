3e11694aa00a5deca0c7da86c6bde267fdc16f6c
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-06-27 01:38:28 +0530

    Time: 25 ms (60.79%), Space: 23.9 MB (98.33%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,30 +11,20 @@
  */
 class Solution {
 public:
-  //we can traverse tree store value of node after that we can sort the array and return the kth from beginning. It will take O(n) space and time complexity will be O(n)+O(nlogn)i.e traverse+sorting
- //if we do inorder traversal T.C. will be only O(n) as inorder is already sorted for BST
-    
-    int kthSmallest(TreeNode* root, int k) {
-        stack<TreeNode*>st;
-        TreeNode* node=root;
-        int count=0; //we will count node during inorder traversal and increment it if we get a node, as it goes in sorted way so when count==k we will return that node.
-        
-        while(true){
-            if(node!=nullptr){
-                st.push(node);
-                node=node->left;
-            }
-            else{
-                if(st.empty())
-                    break;
-                node=st.top();
-                st.pop();
-                count++;
-                if(count==k)
-                    return node->val;
-                node=node->right;
-            }
+    int ans=0;
+    void find(TreeNode* root, int& k){
+        if(root==nullptr)
+            return;
+        find(root->left,k);
+        k--;
+        if(k==0){
+            ans= root->val;
+            return;
         }
-        return -1;
+	    find(root->right,k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        find(root,k);
+        return ans;
     }
 };
