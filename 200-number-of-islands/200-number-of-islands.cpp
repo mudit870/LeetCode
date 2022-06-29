555aaeb45275bc7982458f5a1158f4424af33ba8
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-06-29 12:16:31 +0530

    Time: 36 ms (93.01%), Space: 12.3 MB (58.50%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int r,int c, vector<vector<int>>& dir){
-        int n=grid.size();
-        int m=grid[0].size();
-        grid[r][c]='0';
-        for(auto d:dir){
-            int sr=r+d[0];
-            int sc=c+d[1];
-            if(sr>=0 && sc>=0 && sr<n && sc<m && grid[sr][sc]=='1'){
-                dfs(grid,sr,sc,dir);
-            }
+    bool isValid(int i,int j,int n,int m){
+        if(i<0 || j<0 || i>=n || j>=m){
+            return false;
         }
+        return true;
+    }
+    void make_island(int i,int j,int n,int m,vector<vector<char>>& grid){
+        if(!isValid(i,j,n,m) || grid[i][j]!='1'){
+            return;
+        }
+        grid[i][j] = '0';
+        make_island(i-1,j,n,m,grid);
+        make_island(i+1,j,n,m,grid);
+        make_island(i,j-1,n,m,grid);
+        make_island(i,j+1,n,m,grid);
     }
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
         int n=grid.size();
         int m=grid[0].size();
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
+                if(grid[i][j] == '1'){
+                    make_island(i,j,n,m,grid);
                     ans++;
-                    dfs(grid,i,j,dir);
                 }
             }
         }
