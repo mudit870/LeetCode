37dd6db19a9f7e064ab4c4a94bb9355384c78bd5
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-07-13 13:17:53 +0530

    Time: 19 ms (99.39%), Space: 22.4 MB (9.27%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int low=0;
-        int high=nums.size()-1;// imp.
-        if(nums.size()==1)
-            return nums[0];
-        //even and odd indexes should be same as index start from 0
-        //if even index and it's next index same, move low to mid+1 
+        int high=nums.size()-1;
+        int ans=0;
+        if(nums.size()==1)return nums[0];
+        // [3,3,7,7,10,11,11]
+        //  0,1,2,3,4, 5, 6
+        
         while(low<=high){
             int mid=low+(high-low)/2;
-            cout<<mid<<" "<<low<<" "<<high<<" "<<endl;
-            if(mid%2==0 && nums[mid]==nums[mid+1]){
-            //search right side
-                low=mid+1;
-            }else if(mid%2!=0 && nums[mid]==nums[mid-1]){
-                // search right side
-                low=mid+1;
+            int temp=nums[mid];
+            cout<<temp<<" ";
+            if(mid==0 || mid==nums.size()-1)return nums[mid];
+            else if(nums[mid-1]!=temp && nums[mid+1]!=temp){
+                ans=nums[mid];
+                break;
+            }else if(nums[mid-1]==temp){
+                if(mid%2!=0){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
             }else{
-                high=mid-1;
+                if((nums.size()-mid)%2==0){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
             }
         }
-        return nums[low];
+        return ans;
     }
 };
