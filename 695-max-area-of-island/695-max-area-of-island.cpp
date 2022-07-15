200145972b30e3b99be9880518fb8ae671ad42d0
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-07-15 12:21:29 +0530

    Time: 62 ms (7.60%), Space: 32.3 MB (5.06%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid, vector<vector<int>>& dir, int sr, int sc, int n, int m){
-        int size=0;
+    int bfs(vector<vector<int>>& grid,vector<vector<int>>& dir,int sr, int sc){
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>mq;
+        mq.push({sr,sc});
         grid[sr][sc]=0;
-        for(auto d:dir){
-            int r=sr+d[0];
-            int c=sc+d[1];
-            
-            if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==1){
-                size+=dfs(grid,dir,r,c,n,m);
+        int size=0;
+        while(!mq.empty()){
+            int currRow=mq.front().first;
+            int currCol=mq.front().second;
+            mq.pop();
+            for(auto d:dir){
+                int r=currRow+d[0];
+                int c=currCol+d[1];
+                
+                if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==1){
+                    grid[r][c]=0;
+                    size++;
+                    mq.push({r,c});
+                }
             }
         }
         return size+1;
@@ -21,8 +32,8 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    int area=dfs(grid,dir,i,j,n,m);
-                    ans=max(ans,area);
+                    int area=bfs(grid,dir,i,j);
+                    ans=max(area,ans);
                 }
             }
         }
