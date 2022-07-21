ea2d71c8830d4649441878dcfc39713119321ea5
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-07-22 00:39:56 +0530

    Time: 45 ms (26.94%), Space: 8.8 MB (37.60%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,56 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        set<char>st;
-        int ans=0;
-        int j=0,i=0;
-        for(;i<s.length();i++){
-            if(st.count(s[i])==0){
-                st.insert(s[i]);
-                ans=max(ans,(int)st.size());
+        // unordered_map<char,int>mp;
+        // int ans=INT_MIN;
+        // int count=0;
+        // for(int i=0;i<s.length();i++){
+        //     if(mp.find(s[i])==mp.end()){
+        //         mp[s[i]]=i;
+        //         count++;
+        //     }else{
+        //         ans=max(ans,count);
+        //         count=count-mp[s[i]];
+        //         int j=mp[s[i]];
+        //         mp.erase(s[j]);
+        //         while(j--){
+        //             mp.erase(s[j]);
+        //         }
+        //         mp[s[i]]=i;
+        //     }   
+        // }
+        // ans=max(ans,count);
+        // return ans;
+        
+        int N = s.size();
+        int ans = 0;
+        
+        int i=0;
+        int j=0;
+        unordered_map<char,int> mpp;
+        
+        while(j<=N-1){
+            //calcs
+            mpp[s[j]]++;
+            
+            if(mpp.size() == j-i+1){
+                //ans
+                ans = max(ans, j-i+1);
+               
+                j++;
             }
             else{
-                st.erase(s[j]);
+                //map size is less 
+                //it can never be map size > window 
+                char ch = s[i]; // to be rem
+                mpp[ch]--;
+                if(mpp[ch] == 0){
+                    mpp.erase(ch); // to reduce size;
+                }
+                i++;
                 j++;
-                i--;
             }
+            
         }
         return ans;
     }
