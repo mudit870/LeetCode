f401c5eafb48b480af9db06c4e88472508163efa
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-07-27 02:36:57 +0530

    Time: 357 ms (7.98%), Space: 65.5 MB (5.01%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -4,21 +4,21 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         queue<pair<int,int>>que;
-        int one=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     que.push({i,j});
-                    one++;
-                }else{
-                    grid[i][j]=INT_MAX;
                 }
             }
         }
-        if(que.size()==0 || one==n*m)return -1;
+        //agar ek bhi 1 nahi hai ya saare 1 hai grid me
+        if(que.size()==0 || que.size()==n*m)
+            return -1;
         vector<vector<int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
+        int time=-1;
         while(!que.empty()){
             int size=que.size();
+            time++;
             while(size--){
                 int row=que.front().first;
                 int col=que.front().second;
@@ -28,20 +28,13 @@ public:
                     int r=row+d[0];
                     int c=col+d[1];
                     
-                    if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==INT_MAX){
-                        grid[r][c]=min(grid[r][c],grid[row][col]+1);
+                    if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==0){
+                        grid[r][c]=1;
                         que.push({r,c});
                     }
                 }
             }
         }
-        
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans=max(ans,grid[i][j]);
-            }
-        }
-        return ans-1;
+        return time;
     }
 };
