3f0cb5e15fa00b45a0a6ef3725689b20e076decc
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-07-28 11:31:44 +0530

    Time: 10 ms (74.53%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>arr(26,0);
-        for(auto e:s){
-            arr[e-'a']++;
-        }
-        for(auto e:t){
-            arr[e-'a']--;
-        }
-        for(auto e:arr)
-            if(e>0 || e<0)
-                return false;
+        unordered_map<char,int>mp;
+        for(auto e:s)
+            mp[e]++;
+        for(auto e:t)
+            mp[e]--;
+        for(auto e:mp)
+            if(e.second!=0)return false;
         return true;
     }
 };
