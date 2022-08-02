a7cb795cb4b9071decbe430cfb04bc4796a1eada
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-08-02 16:31:38 +0530

    Time: 368 ms (66.73%), Space: 56.1 MB (91.30%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,20 +1,36 @@
 class Solution {
-public:
-    static bool comparator(vector<int>& c1,vector<int>& c2){
-        return c1[1]<c2[1];
+    public:
+    static bool cmp(vector<int>& a, vector<int>& b){
+        return a[1]<b[1];
     }
+public:
     int scheduleCourse(vector<vector<int>>& courses) {
-        sort(courses.begin(),courses.end(),comparator);
-        int ans=0;
-        priority_queue<int>pq;
-        for(int i=0;i<courses.size();i++){
-            pq.push(courses[i][0]);
-            ans+=courses[i][0];
-            if(ans>courses[i][1]){
-                ans-=pq.top();
+        sort(courses.begin(),courses.end(),cmp);
+        // for(auto e: c){
+        //     cout<<e[0]<<" "<<e[1]<<endl;
+        // }
+        priority_queue<int> pq;  //build a max heap
+        int sum=0;
+        int n=courses.size();
+        for(int i=0;i<n;i++){
+            pq.push(courses[i][0]);  
+            //push duration of course in max heap and add it to the sum
+            sum+=courses[i][0];
+
+            if(sum>courses[i][1]){  //if at any instance, sum becomes greater than last day for a particula course, then pop and subtract it's duration from the sum
+                sum=sum-pq.top();
                 pq.pop();
             }
         }
-        return pq.size();
+        return pq.size();  //return pq.size
     }
 };
+// 200,100
+// 1250,1000
+// 1300,200
+// 3200,2000
+    
+// 0
+// 0+100
+// 100 +1000
+// 1100+200
