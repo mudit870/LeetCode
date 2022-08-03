0d97b3529c96e858498252fe12253a9b34337016
Mudit Sharma <84176287+mudit870@users.noreply.github.com>
2022-08-04 01:49:08 +0530

    Time: 16 ms (72.43%), Space: 8.9 MB (35.89%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,56 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // unordered_map<char,int>mp;
-        // int ans=INT_MIN;
-        // int count=0;
-        // for(int i=0;i<s.length();i++){
-        //     if(mp.find(s[i])==mp.end()){
-        //         mp[s[i]]=i;
-        //         count++;
-        //     }else{
-        //         ans=max(ans,count);
-        //         count=count-mp[s[i]];
-        //         int j=mp[s[i]];
-        //         mp.erase(s[j]);
-        //         while(j--){
-        //             mp.erase(s[j]);
-        //         }
-        //         mp[s[i]]=i;
-        //     }   
-        // }
-        // ans=max(ans,count);
-        // return ans;
-        
-        int N = s.size();
         int ans = 0;
-        
-        int i=0;
-        int j=0;
-        unordered_map<char,int> mpp;
-        
-        while(j<=N-1){
-            //calcs
-            mpp[s[j]]++;
-            
-            if(mpp.size() == j-i+1){
-                //ans
-                ans = max(ans, j-i+1);
-               
-                j++;
-            }
-            else{
-                //map size is less 
-                //it can never be map size > window 
-                char ch = s[i]; // to be rem
-                mpp[ch]--;
-                if(mpp[ch] == 0){
-                    mpp.erase(ch); // to reduce size;
-                }
+        unordered_set<char> st;
+        for(int i=0,j=0;j<s.size();j++){
+            if(st.count(s[j])){
+                while(s[i] != s[j])
+                    st.erase(s[i++]);
                 i++;
-                j++;
             }
-            
+            st.insert(s[j]);
+            ans = max(ans,j-i+1);
         }
         return ans;
     }
